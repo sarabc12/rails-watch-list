b487de07644999ba4dafb87205b93ea98f4f4c1f
sara burchi colonna <burchicolonnasara@gmail.com>
2025-03-06 16:53:18 +0100

    WatchList

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "lists#index"
+
+  resources :lists, except: [:edit, :update] do
+   resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
 end
