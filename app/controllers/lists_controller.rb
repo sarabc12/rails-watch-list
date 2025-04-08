2d1a8cbda2459a17f2c18961f76e1a1e4e1bedb6
sara burchi colonna <burchicolonnasara@gmail.com>
2025-04-08 12:02:22 +0200

    Add random to home page for movies

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
-    @movies = Movie.all.limit(30)
+    @movies = Movie.order("RANDOM()").limit(30)
   end
 
   def show
