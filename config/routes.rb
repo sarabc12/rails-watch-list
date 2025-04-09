c6c12c69050046564d179b073ac883a77176ad54
Jose Oliveira <jedpfo89@gmail.com>
2025-04-09 10:51:01 +0100

    commenting code is against my art

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "lists", to: "lists#lists", as: :lists_index
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
   resources :lists, except: [:edit, :update] do
    resources :bookmarks, only: [:new, :create]
   end
-
   resources :bookmarks, only: [:destroy]
 
 end
